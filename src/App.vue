cd76f19a24177be0031bac9e2762f67952a9191a
meta-ela <anxhelameta@live.it>
2022-06-24 14:50:27 +0200

    create TheHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TheHeader
+}
 }
 </script>
 
 <style lang="scss">
 
-@import "./assets/scss/main.scss";
+@import "assets/scss/main.scss";
 
 </style>
